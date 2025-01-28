7aab7163a39adc8ebddba2ba8383ce9bbe27246c
Kirti Shilwant <shilwantkirti@gmail.com>
2025-01-28 23:51:01 +0530

    Initial Commit

---
+++ ElevatorTests/ElevatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorTests.swift
+//  ElevatorTests
+//
+//  Created by KirtiS on 1/28/25.
+//
+
+import Testing
+@testable import Elevator
+
+struct ElevatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
