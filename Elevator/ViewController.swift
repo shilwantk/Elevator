a465da5e300a99f9af27749acc94d7ed835c4e40
Kirti Shilwant <shilwantkirti@gmail.com>
2025-02-03 15:31:49 +0530

    Add code to set up elevator instance

--- Elevator/ViewController.swift
+++ Elevator/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let elevator = Elevator(maxFloors: 15)
+        elevator.request(stop: ElevatorStop(startFloor: 5, endFloor: 7))
+        elevator.request(stop: ElevatorStop(startFloor: 1, endFloor: 2))
+        elevator.request(stop: ElevatorStop(startFloor: 4, endFloor: 2))
+        elevator.request(stop: ElevatorStop(startFloor: 7, endFloor: 9))
+        elevator.request(stop: ElevatorStop(startFloor: 3, endFloor: 2))
+        elevator.move()
+    }
 }
 
